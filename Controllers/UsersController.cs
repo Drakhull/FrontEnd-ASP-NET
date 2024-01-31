73d58bbc5f11cae36dc9673a415121acd211cfbb
iurig <iurig@DAEDRA>
2024-01-31 20:21:34 -0300

    Initial Commit

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -47,32 +47,34 @@ namespace Telessaude.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateConfirmation(User user)
         {
-            // Lógica para enviar os dados para a API Java
-            // Utilize HttpClient para fazer a requisição POST
-            // Certifique-se de tratar os casos de sucesso e falha adequadamente
-
-            HttpClient client = new HttpClient
+            try
             {
-                BaseAddress = new Uri("http://localhost:8080")
-            };
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:8080");
 
-            var serializedUser = JsonConvert.SerializeObject(user);
-            var content = new StringContent(serializedUser, Encoding.UTF8, "application/json");
+                    var serializedUser = JsonConvert.SerializeObject(user);
+                    var content = new StringContent(serializedUser, Encoding.UTF8, "application/json");
 
-            var response = await client.PostAsync("users", content);
+                    var response = await client.PostAsync("users", content);
 
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
             }
-            else
+            catch (Exception ex)
             {
                 return View("Error");
             }
         }
         public IActionResult DeleteConfirmation(String id, String name)
         {
-            // Passe ambos os parâmetros para a view
             ViewData["UserId"] = id;
             ViewData["UserName"] = name;
 
